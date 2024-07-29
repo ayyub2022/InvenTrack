775391fc194f1b456aef38d5fbb786c75de33863
NguyoVictor <victor.nguyo@student.moringaschool.com>
2024-07-29 16:09:05 +0300

    removed product class

--- server/models.py
+++ server/models.py
@@ -3,13 +3,6 @@ from sqlalchemy.ext.associationproxy import association_proxy
 
 from config import db
 
-class Product(db.Model, SerializerMixin):
-    prod_id = db.Column(db.Integer, primary_key= True)
-    prod_name = db.Column(db.String(20), nullable = False)
-    prod_name = db.Column(db.String(20),unique = True ,nullable = False)
-    prod_qty = db.Column(db.Integer, nullable = False)
-    def __repr__(self):
-        return f"Product('{self.prod_id}','{self.prod_name}','{self.prod_qty}')"
 
 class item(db.model, SerializerMixin):
     id = db.Column(db.Integer, primary_key=True)
