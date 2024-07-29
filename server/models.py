6b10e928485aeef776cf548ec104333c4a823ec5
NguyoVictor <victor.nguyo@student.moringaschool.com>
2024-07-29 15:21:25 +0300

    initial commit for product class

--- server/models.py
+++ server/models.py
@@ -3,3 +3,10 @@ from sqlalchemy.ext.associationproxy import association_proxy
 
 from config import db
 
+class Product(db.Model, SerializerMixin):
+    prod_id = db.Column(db.Integer, primary_key= True)
+    prod_name = db.Column(db.String(20), nullable = False)
+    prod_name = db.Column(db.String(20),unique = True ,nullable = False)
+    prod_qty = db.Column(db.Integer, nullable = False)
+    def __repr__(self):
+        return f"Product('{self.prod_id}','{self.prod_name}','{self.prod_qty}')"
