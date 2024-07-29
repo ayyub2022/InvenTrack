a0265279a1ae1d82c1eb96068ceda9b2ec03a32e
ayyub2022 <ayyubibrahim22@gmail.com>
2024-07-29 15:29:18 +0300

    created transaction model

--- server/models.py
+++ server/models.py
@@ -3,19 +3,12 @@ from sqlalchemy.ext.associationproxy import association_proxy
 
 from config import db
 
-
-class products (db.models,SerializerMixin);
-    __tablename__ = 'products'
+class Transaction(db.Model):
+    __tablename__ = 'transactions'
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(255), nullable=False)
-    price = db.Column(db.Float, nullable=False)
+    inventory_id = db.Column(db.Integer, db.ForeignKey('inventory.id'), nullable=False)
+    transaction_type = db.Column(db.String(50), nullable=False)
     quantity = db.Column(db.Integer, nullable=False)
-    category_id = db.Column(db.Integer, db.ForeignKey('categories.id'))
-    category = db.relationship('categories', backref=db.backref('products', lazy=True))
-    orders = association_proxy('order_items', 'product')
-    # Add more columns as needed!
-    def __repr__(self):
-        return f'<Product {self.name}>'
-class shop(db.models,SerializerMixin)
-    id = db.column(db.integer,primary_key=True)
-# Models go here!
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+
