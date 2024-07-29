8b71a11bde3af4575cff7ce6585cf4b7d96be979
ayyub2022 <ayyubibrahim22@gmail.com>
2024-07-29 15:32:20 +0300

    created payment  model

--- server/models.py
+++ server/models.py
@@ -12,3 +12,10 @@ class Transaction(db.Model):
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
 
 
+class Payment(db.Model):
+    __tablename__ = 'payments'
+    id = db.Column(db.Integer, primary_key=True)
+    inventory_id = db.Column(db.Integer, db.ForeignKey('inventory.id'), nullable=False)
+    amount = db.Column(db.Numeric, nullable=False)
+    payment_date = db.Column(db.DateTime, default=datetime.utcnow)
+
