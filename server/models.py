fab6279937b9fd62defd94fdd433d7ec6090fc67
partycrasher546 <muthonidennis546@gmail.com>
2024-07-29 15:12:42 +0300

    user model complete

--- server/models.py
+++ server/models.py
@@ -3,19 +3,11 @@ from sqlalchemy.ext.associationproxy import association_proxy
 
 from config import db
 
-
-class products (db.models,SerializerMixin);
-    __tablename__ = 'products'
+# Define your models here!
+class user(db.model,SerializerMixin):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(255), nullable=False)
-    price = db.Column(db.Float, nullable=False)
-    quantity = db.Column(db.Integer, nullable=False)
-    category_id = db.Column(db.Integer, db.ForeignKey('categories.id'))
-    category = db.relationship('categories', backref=db.backref('products', lazy=True))
-    orders = association_proxy('order_items', 'product')
-    # Add more columns as needed!
-    def __repr__(self):
-        return f'<Product {self.name}>'
-class shop(db.models,SerializerMixin)
-    id = db.column(db.integer,primary_key=True)
-# Models go here!
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), nullable=False, unique=True)
+    password = db.Column(db.String(100), nullable=False)
+    role = db.Column(db.string(100),nullable=False)
+  
