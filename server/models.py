983a7c0d127ec8ab4ad85762989c50d541a76dc3
partycrasher546 <muthonidennis546@gmail.com>
2024-07-29 12:51:09 +0300

    test

--- server/models.py
+++ server/models.py
@@ -3,4 +3,9 @@ from sqlalchemy.ext.associationproxy import association_proxy
 
 from config import db
 
+
+class products (db.models,SerializerMixin);
+    __tablename__ = 'products'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
 # Models go here!
