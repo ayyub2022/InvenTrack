5fc1b3ff65fdb32ac74286b3adbb549141e2623c
Edmound Njoori <eddnjoori@gmail.com>
2024-07-29 15:19:20 +0300

    Complete suppliers table.

--- server/models.py
+++ server/models.py
@@ -3,3 +3,9 @@ from sqlalchemy.ext.associationproxy import association_proxy
 
 from config import db
 
+class Supplier(db.Model):
+    __tablename__ = 'suppliers'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    contact_info = db.Column(db.Text, nullable=False)
+
