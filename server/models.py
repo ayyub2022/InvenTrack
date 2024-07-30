29c6248160035b9254cdfc36e4c14a0cc94b3b24
NguyoVictor <victor.nguyo@student.moringaschool.com>
2024-07-30 08:35:49 +0300

    initializing inventory class with specified attributes

--- server/models.py
+++ server/models.py
@@ -19,17 +19,21 @@ class Product(db.model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(20), unique=True, nullable=False)
-    bp = db.Column(db.Float, nullable=False)  # Buying price
-    sp = db.Column(db.Float, nullable=False)  # Selling price
+    bp = db.Column(db.Float, nullable=False)
+    sp = db.Column(db.Float, nullable=False)
     created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
 class inventory(db.model):
   __tablename__ = 'inventory'
+
   id = db.Column(db.Integer, primary_key=True)
-  name = db.Column(db.String(50), nullable=False)
-  price = db.Column(db.Float, nullable=False)
+  product_id = db.Column(db.Integer, db.ForeignKey('products.id'), nullable=False)
   quantity = db.Column(db.Integer, nullable=False)
-  created_at = db.Column(db.DateTime, default=datetime.utcnow)
+  spoilt_quantity = db.Column(db.Integer, nullable=False, default=0)
+  payment_status = db.Column(db.String(20), nullable=False)
+  created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+  product = db.relationship('Product', backref=db.backref('inventories', lazy=True))
 
 
 class user(db.model,SerializerMixin):
